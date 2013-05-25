c122cc2c8a723a2d58e2e621471f37446d8cafdb
Arne Noehre <arne.noehre@dff-solutions.de>
2013-05-25 19:06:38 +0200

    Lizent dazu und NuGet Package vorbereitet

---
+++ dff.Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dff.Extensions.HelperClasses;
+
+namespace dff.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Git für ein beliebiges Objekt einen kompletten Dump aus, der alle Ebenen überspannt.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string Dump(this Object obj)
+        {
+            return ObjectDumper.Dump(obj);
+        }
+    }
+}
---
+++ dff.Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dff.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static string GetDescription(this TimeSpan ts)
+        {
+            var diff = ts.Ticks > 0 ? "vor" + " " : "in" + " ";
+            var min = ts.Minutes;
+            var std = ts.Hours;
+            var tage = ts.Days;
+            if (tage == 0)
+            {
+                if (std > 1) diff += std + " " + "Stunden" + " ";
+                else if (std == 1) diff += std + " " + "Stunde" + " ";
+                if (min == 0) diff += min + " " + "Minute";
+                else diff += min + " " + "Minuten";
+            }
+            else
+            {
+                if (tage == 1) diff += tage + " " + "Tag";
+                else diff += tage + " " + "Tagen";
+            }
+            return diff;
+        }
+    }
+}
